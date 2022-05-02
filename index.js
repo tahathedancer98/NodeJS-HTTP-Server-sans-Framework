c07c7f365fc8b13d1fa781a81d72fadeb375b62f
tahathedancer98 <bookingtaha98@gmail.com>
2022-05-02 16:19:04 +0200

    Q5 - Erreur Interne au Serveur

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const http = require('http');
 
 function handler(req, res) {
     console.log(req)
-    if(req.url === '/'  && req.method != "GET"){
-        res.writeHead(200, {'Content-type':'text/plain'});
-        res.write(`<h1>${req.statusCode} Méthode non authorisée`);
+    try{
+        if(req.url === '/'  && req.method != "GET"){
+            res.writeHead(200, {'Content-type':'text/plain'});
+            res.write(`<h1>${req.statusCode} Méthode non authorisée`);
+            res.end();
+        }else {
+            res.writeHead(404, {'Content-type':'text/plain'});
+            res.write(`<h1>${req.statusCode} Page Introuvable</h1>`);
+            res.end();
+        };
+    }catch(e){
+        res.writeHead(500, {'Content-type':'text/plain'});
+        res.write(`<h1>${req.statusCode} Erreur Interne au Serveur</h1>`);
         res.end();
-    }else {
-        res.writeHead(404, {'Content-type':'text/plain'});
-        res.write(`<h1>${req.statusCode} Page Introuvable</h1>`);
-        res.end();
-    };
+    }
+    
 }
 
 const server = http.createServer(handler);
