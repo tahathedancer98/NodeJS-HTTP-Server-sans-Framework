98da09cbe1d9c5de417838ecc11d40de1cefa080
tahathedancer98 <bookingtaha98@gmail.com>
2022-05-02 15:42:16 +0200

    Q2 - L'ajoutet de la aroute / avec la methode GET

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.write('<h1>HELLO WORLD TAHA </h1>')
+const server = http.createServer();
+
+app.get('/', function(req, res) {
+    res.send('<h1>HELLO WORLD TAHA </h1>')
 });
-
 server.listen(5000);
+console.log('Serveur écoute sur le port 5000');
